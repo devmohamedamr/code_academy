16208ec95406ed8921a986638f8be9c413b95f76
mohamed amr <dev.mohamedamr@gmail.com>
2023-07-17 13:08:34 +0400

    session 1

---
+++ session2/script.js
@@ -0,0 +1,81 @@
+// $("#click").click(()=>{
+//     $(".box").slideUp(1000,()=>{
+//         $(".box").show(1000,()=>{
+//             $(".box").fadeOut(1000)
+//         })
+//     })
+//     // $(".top").animate({width:"100%"},4000)
+// })
+
+
+// $("#stop").click(()=>{
+//     $(".top").stop() 
+// })
+
+
+// $("#click").click(()=>{
+//     $(".box").hide(1000,()=>{
+//         $(".box").slideDown(1000,()=>{
+//             $(".box").fadeOut(1000)
+//         })
+//     })
+// })
+
+// $("#click").click(()=>{
+//     $(".box").hide(1000).slideDown(1000).fadeOut(1000)
+// })
+
+
+// $("#calc").click(()=>{
+//     let x = $("#x").val("mohamed")
+//     let y = $("#y").val("amr")
+//     // $("#res").text("this result is :"+ x+y)
+//     $("#res").text(`this result is : ${+x + +y}`)
+// })
+
+
+// $.get({
+//     url:"https://jsonplaceholder.typicode.com/users",
+//     success:function(res){
+//         let text = ""
+//         for(let i = 0;i<res.length;i++){
+//             text += `<li>${res[i].name}</li>`
+//         }
+//         $("#list").html(text)
+//     },
+//     error:function(res){
+//         console.log(res)
+//     }
+// })
+
+
+
+// $.get({
+//     url:"https://jsonplaceholder.typicode.com/users",
+//     success:function(res){
+//        let text =  res.map((user)=> `<li> ${user.name} </li>` )
+//        $("#list").html(text)
+//     }
+// })
+
+
+
+// $.get({
+//     url:"https://jsonplaceholder.typicode.com/users",
+//     success:function(res){
+//         let text = ""
+//         res.forEach((value,key)=>{
+//             text += `<li>${value.name}</li>`
+//         })
+//         $("#list").html(text)
+//     }
+// })
+
+
+$.get({
+    url:"https://jsonplaceholder.typicode.com/users",
+    success:function(res){
+      let text=   res.map((x)=>`<li> ${x.name} </li>`)
+      $("#list").html(text)
+    }
+})
